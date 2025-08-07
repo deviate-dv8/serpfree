51b4a624b6bece44e935cab076d2274c744b5968
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-07 14:11:11 +0800

    added logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import SERPScraper from "./scraper-provider";
 import routes from "./routes";
+import si from "systeminformation";
+
 // Initialize
 dotenv.config();
 const app = express();
@@ -13,6 +15,16 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+async function logUsage() {
+  const [mem, cpu] = await Promise.all([si.mem(), si.currentLoad()]);
+  console.clear();
+  console.log(
+    `Memory: ${(mem.used / 1024 / 1024).toFixed(2)} MB used / ${(mem.total / 1024 / 1024).toFixed(2)} MB`,
+  );
+  console.log(`CPU Load: ${cpu.currentLoad.toFixed(2)}%`);
+}
+
+setInterval(logUsage, 1000);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to SERP Scraper API" });
 });
